81a7f8af71f415caff1deed177ece5cde513481f
Nikita Andriyanov <nikita-and-nov@mail.ru>
2015-04-10 20:56:19 +0300

    Modified Version 10 april
    Version 2 Validate

--- queryClass.php
+++ queryClass.php
@@ -1,34 +1,4 @@
 <?
-/*
-class queryString {
-	protected $query;
-	
-	public function __construct($query) {
-		$this->query=$query;
-	}
-	
-	public function getQuery() {
-		return $this->query;
-	}
-	
-	public function select($columns) {
-	$query="SELECT ";
-	foreach($columns as $key=>$value) {
-		$query=$query."'$value', ";
-	}
-	$this->query=substr($query,0,-2);
-			
-	return new queryString($this->query);
-	}
-	
-	public function from($tablename) {
-		$query=" FROM $tablename";
-		return $query;
-	}
-	
-}
-*/
-
 class queryClass {
 	protected $type;
 	protected $host;
@@ -45,22 +15,15 @@ class queryClass {
 		$this->password=$config["password"];
 		$this->database=$config["database"];
 		
-		//try {
 			if ($this->type=='mysql') {
-				$link=mysql_connect($config['host'],$config['username'],$config['password']) or die('Нет соединения');//throw new NotConnectException('Нет соединения с Сервером');
-				//mysql_query ("CREATE DATABASE IF NOT EXISTS ".$database) or die ("Не могу создать базу данных!");
-				//echo $host;
+				$link=mysql_connect($config['host'],$config['username'],$config['password']) or die('Нет соединения с сервером');//throw new NotConnectException('Нет соединения с Сервером');
 				mysql_select_db($config['database']) or die ('Не удалось подключиться к БД');
-				//$MySqlQuery=new queryString("");
-				//return $MySqlQuery;
 			}
-		//	catch (NotConnectException $e) {
-		//		echo $e->getMessage();
-		//	}
-		//}
+			if ($this->type=='mssql') {
+				$link=mssql_connect($config['host'],$config['username'],$config['password']) or die('Нет соединения с сервером');
+				mssql_select_db($config['database']) or die ('Не удалось подключиться к БД');
+			}
 	}
-	
-	
 	
 	public function getConfig() {
 		$config["type"]=$this->type;
@@ -77,33 +40,58 @@ class queryClass {
 	}
 	
 	public function select($columns) {
+		
 		$query="SELECT ";
-	foreach($columns as $key=>$value) {
-		if ($this->type=='mysql') {
-			$query=$query.mysql_real_escape_string($value).", ";
+		foreach($columns as $key=>$value) {
+			if ($value!='*') {
+				$query=$query."`$value`, ";
+			}
+			else {
+				$query=$query."$value, ";
+			}
 		}
-		echo mysql_real_escape_string($value)."<br/>";
-	}
-	
-	$query=substr($query,0,-2);
-	$this->query.=$query;
-	return $this;
+		$query=substr($query,0,-2);
+		$this->query.=$query;
+		return $this;
 	}
 	
 	public function from($tablename) {
-		$query=" FROM $tablename";
+		$query=" FROM `$tablename`";
 		$this->query.=$query;
 		return $this;
 	}
 	
 	public function where($conditions) {
+		
 		$query=" WHERE ";
-		foreach ($conditions as $key=>$array) {
-			foreach ($array as $value) {
-				$query=$query."$value ";
+		foreach($conditions as $key=>$array) {
+			$i=0;
+			foreach($array as $value) {
+				if(get_magic_quotes_gpc()==1) {
+					$value=stripslashes(trim($value));
+				}
+				else
+				{
+					$value=trim($value);
+				}
+
+				switch($i) {
+					case 0:  {	$query=$query."`$value` "; $i++; break;}
+					case 1:  {	$query=$query."$value "; $i++; break;}
+					case 2:  { 	
+						$value=mysql_real_escape_string($value);
+						$query=$query."'$value' "; 
+						$i++; 
+						break;
+					}
+					default: {	$query=$query."$value "; }
+				}
+				if (sizeOf($array)==3) {
+					$query=substr($query,0,-1);
+				}
 			}
 		}
-		//$query=substr($query,0,-2);
+		
 		$this->query.=$query;
 		return $this;
 	}
@@ -123,12 +111,10 @@ class queryClass {
 		return $this;
 	}
 	
-	
-	
 	public function insert($table, $columns) {
-		$query="INSERT INTO $table (";
+		$query="INSERT INTO `$table` (";
 		foreach ($columns as $key=>$value) {
-			$query.="$value,";
+			$query.="`$value`,";
 		}
 		$query=substr($query,0,-1);
 		$query.=")";
@@ -139,16 +125,25 @@ class queryClass {
 	public function values($values) {
 		$query=" VALUES (";
 		foreach ($values as $key=>$value) {
-			$query.="'$value',";
+			if(get_magic_quotes_gpc()==1) {
+				$value=stripslashes(trim($value));
+			}
+			else
+			{
+				$value=trim($value);
+			}
+
+			$value=mysql_real_escape_string($value);
+			$query=$query."'$value', "; 
 		}
-		$query=substr($query,0,-1);
+		$query=substr($query,0,-2);
 		$query.=")";
 		$this->query.=$query;
 		return $this;
 	}
 	
 	public function update($table) {
-		$query="UPDATE $table ";
+		$query="UPDATE `$table` ";
 		$this->query.=$query;
 		return $this;
 	}
@@ -156,15 +151,23 @@ class queryClass {
 	public function set($setvalues) {
 		$query="SET ";
 		foreach ($setvalues as $key=>$value) {
-			$query.="$key='$value', ";
+			if(get_magic_quotes_gpc()==1) {
+				$value=stripslashes(trim($value));
+			}
+			else
+			{
+				$value=trim($value);
+			}
+			$value=mysql_real_escape_string($value);
+			$query.="`$key`='$value', ";
 		}
 		$query=substr($query,0,-2);
 		$this->query.=$query;
 		return $this;
 	}
 	
-	public function delete($tablename) {
-		$query="DELETE FROM $tablename";
+	public function deletefrom($tablename) {
+		$query="DELETE FROM `$tablename`";
 		$this->query.=$query;
 		return $this;
 	}
@@ -172,8 +175,11 @@ class queryClass {
 	public function save() {
 		if ($this->type=='mysql') {
 			$this->result=mysql_query($this->query);
-			$this->query="";
 		}
+		if ($this->type=='mssql') {
+			$this->result=mssql_query($this->query);
+		}
+		$this->query="";
 	}
 	public function getResult() {
 		return $this->result;
@@ -188,43 +194,46 @@ $conf=array(
 	"database"=>"testdb"
 	);
 	
-//var_dump($conf);
-//echo $conf["type"];
 $db=new queryClass($conf);
-//print_r($db);
 
 $cols=array("name", "country");
 $table="teams";
-$conditions=array(array("id",">",3,"OR"),array("country","<>","'ukraine'"));
+$conditions=array(array("id","<>",3,'AND'),array('country','<>','ukraine'));
 $values=array("real","spain");
 $setvalues=array(
 "name"=>"chelsea",
 "city"=>"london");
-//$query=$db->select($cols).$db->from($table).$db->where($conditions);
-//$db->select($cols);
-//$db->from($table);
-//$db->insert($table, $cols);
-//$db->values($values);
-//$db->save();
-//echo $db->from($table);
-//$db->update($table);
-//$db->set($setvalues);
-//$db->where(array(array("name","=","'manchester united'")));
-//$db->delete("teams");
-//$db->where(array(array("id","=",2)));
-//$db->save();
 
 $db->select(array("*"))->from("teams")
-->where(array(array("name","LIKE","'%csk%'","AND"),array("country","<>","'ukraine'")))
+//->where($conditions);
+//->where(array(array("name","=","-1' UNION SELECT `name`, `lastname` FROM coaches WHERE id='1","AND"),array("name","<>","ukraine")));
+//->where(array(array("name","=","-1';DELETE FROM `teams` WHERE city='london")));
+->where($conditions);
 //->limit(5)
-->orderby("country","DESC");
-
-
-echo $db->getQuery();
-
+//->orderby("name","DESC")
+//->limit(1);
+echo $db->getQuery()."<br>";
 $db->save();
+
+$db->insert($table,array("name","country","city"))
+->values(array("zenit","russia","St. Petersburg"));
+echo $db->getQuery()."<br>";
+$db->save();
+
+$db->update($table)
+->set(array("name"=>"arsenal"))
+->where(array(array("city","=","london")));
+echo $db->getQuery()."<br>";
+$db->save();
+
+$db->deletefrom("teams")
+->where(array(array("id",'>',6)));
+echo $db->getQuery();
+$db->save();
+
 $result=$db->getResult();
 
+/*
 if (!$result) {
     echo "Could not successfully run query ($query) from DB: " . mysql_error();
     exit;
@@ -235,19 +244,11 @@ if (mysql_num_rows($result) == 0) {
     exit;
 }
 
-// До тех пор, пока в результате содержатся ряды, помещаем их в ассоциативный массив.
-// Замечание: если запрос возвращает только один ряд - нет нужды в цикле.
-// Замечание: если вы добавите extract($row); в начало цикла, вы сделаете
-//            доступными переменные $userid, $fullname и $userstatus
+
 while ($row = mysql_fetch_assoc($result)) {
     echo $row["name"];
-    echo $row["country"];
+    echo $row["city"];
+	echo $row["country"]."<br>";
 }
-
-mysql_free_result($result);
-
-//$conn=mysql_connect("localhost","root","");
-//$database = "testdb";
-
-
-?>
+*/
+//mysql_free_result($result);
